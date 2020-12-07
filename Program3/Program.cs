142abe3c628c140c57ef7766ebcf3e0e39fcd2fc
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2020-12-07 12:00:53 +0300

    Добавьте файлы проекта.

---
+++ Program3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime thisDay = DateTime.Today;
+            Console.WriteLine("Date: ");
+            Console.WriteLine(thisDay.ToString("d"));
+            Console.WriteLine(thisDay.AddYears(10).ToString("d"));//plus 10 years
+            Console.Write("Enter the number of years: ");
+            string years = Console.ReadLine();
+            Int32.TryParse(years, out int j);
+            Console.WriteLine(thisDay.AddYears(j).ToString("d"));
+
+
+        }
+    }
+}
